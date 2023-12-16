3e552f76045b6de50a08386a84c3ef5920839cb5
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-12-16 09:38:30 -0500

    Creaciondelproyecto

---
+++ MSproducto/src/main/java/TDA/MSproducto/MSproductoApplication.java
@@ -0,0 +1,13 @@
+package TDA.MSproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSproductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSproductoApplication.class, args);
+	}
+
+}
---
+++ MSproducto/src/test/java/TDA/MSproducto/MSproductoApplicationTests.java
@@ -0,0 +1,13 @@
+package TDA.MSproducto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSproductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
