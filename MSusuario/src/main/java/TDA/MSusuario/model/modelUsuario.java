7685605e71a8b36f7c71c43902567d4977f544d0
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-12-19 13:16:57 -0500

    implementacion del modelo

---
+++ MSusuario/src/main/java/TDA/MSusuario/model/modelUsuario.java
@@ -0,0 +1,50 @@
+package TDA.MSusuario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Seguridad")
+public class modelUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idusuario")
+    private Integer idusuario;    
+
+    @Column(name = "nombreusuario")
+    private String nombreusuario;
+
+    @Column(name = "clave")
+    private String clave;
+
+    public Integer getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(Integer idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombreusuario() {
+        return nombreusuario;
+    }
+
+    public void setNombreusuario(String nombreusuario) {
+        this.nombreusuario = nombreusuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+
+    
+}
