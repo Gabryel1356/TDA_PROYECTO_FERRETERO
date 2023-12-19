e4ea44b9a2a5d1d271442e384c32ff9abe5b1c0a
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-12-19 16:22:59 -0500

    implementacion de personarequest

---
+++ MSpersona/src/main/java/TDA/MSpersona/dto/PersonaRequest.java
@@ -0,0 +1,129 @@
+package TDA.MSpersona.dto;
+
+
+public class PersonaRequest {
+
+    private int idpersona;
+
+    private int idempleado;
+
+    private int tiopodocumento;
+
+    private String nombre;
+
+    private String apepaterno;
+
+    private String apematerno;
+
+    private String fechanacimiento;
+
+    private String telefono;
+
+    private String correo;
+
+    private int sexo;
+
+    private String direccion;
+
+    private int tipopersona;
+
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public int getIdempleado() {
+        return idempleado;
+    }
+
+    public void setIdempleado(int idempleado) {
+        this.idempleado = idempleado;
+    }
+
+    public int getTiopodocumento() {
+        return tiopodocumento;
+    }
+
+    public void setTiopodocumento(int tiopodocumento) {
+        this.tiopodocumento = tiopodocumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApepaterno() {
+        return apepaterno;
+    }
+
+    public void setApepaterno(String apepaterno) {
+        this.apepaterno = apepaterno;
+    }
+
+    public String getApematerno() {
+        return apematerno;
+    }
+
+    public void setApematerno(String apematerno) {
+        this.apematerno = apematerno;
+    }
+
+    public String getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(String fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTipopersona() {
+        return tipopersona;
+    }
+
+    public void setTipopersona(int tipopersona) {
+        this.tipopersona = tipopersona;
+    }
+
+
+
+    
+}
