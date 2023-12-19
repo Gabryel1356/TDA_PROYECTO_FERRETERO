ab6571f0a0589887646e2b504c15a171f76e0c6e
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-12-19 16:24:12 -0500

    implementacion de mensajeria

---
+++ MSpersona/src/main/java/TDA/MSpersona/mensaje/Mensajeria.java
@@ -0,0 +1,33 @@
+package TDA.MSpersona.mensaje;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mensajeria {
+
+    public String MSGELIMEXIT() {
+
+        return "SE ELIMINO CON EXITO EL PRODUCTO";
+    }
+
+    public String MSGEROR() {
+
+        return "SUCEDIO UN ERROR:";
+    }
+
+    public String MSGENCONTAR() {
+
+        return "SE ENCONTRO CON EXITO ";
+    }
+
+    public String MSGEXITO() {
+
+        return "SE GUARDO CON EXITO";
+    }
+
+     public String MSGMODIEXITO() {
+
+        return "SE MODIFICO CON EXITO";
+    }
+
+}
