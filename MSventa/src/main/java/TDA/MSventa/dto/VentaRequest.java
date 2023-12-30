c0f564f314b55fcf61980e855f503a7fadca566a
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-12-29 19:30:49 -0500

    implementacion del modeloventa

--- MSventa/src/main/java/TDA/MSventa/dto/VentaRequest.java
+++ MSventa/src/main/java/TDA/MSventa/dto/VentaRequest.java
@@ -1,87 +1,97 @@
-package TDA.MSventa.model;
+
+package TDA.MSventa.dto;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 
-@Entity
-@Table(name = "venta")
-public class modeloVenta implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class VentaRequest implements Serializable {
 
-    @Column(name = "idventa")
+
     private int idventa;
-
-    @Column(name = "fechaventa")
     private String fechaventa;
-
-    @Column(name = "igv")
     private double igv;
-
-    @Column(name = "subtotal")
     private double subtotal;
-
-    @Column(name = "costoventa")
     private double costoventa;
-
-    @Column(name = "tipocomprobante")
     private String tipocomprobante;
+    
+
+    public VentaRequest() {
+    }
+
+
+    public VentaRequest(int idventa, String fechaventa, double igv, double subtotal, double costoventa,String tipocomprobante) {
+        this.setIdventa(idventa);
+        this.setFechaventa(fechaventa);
+        this.setIgv(igv);
+        this.setSubtotal(subtotal);
+        this.setCostoventa(costoventa);
+        this.setTipocomprobante(tipocomprobante);
+    }
+
 
     public int getIdventa() {
-        return idventa;
+        return idventa; 
     }
+
 
     public void setIdventa(int idventa) {
         this.idventa = idventa;
     }
 
+
     public String getFechaventa() {
         return fechaventa;
     }
+
 
     public void setFechaventa(String fechaventa) {
         this.fechaventa = fechaventa;
     }
 
+
     public double getIgv() {
         return igv;
     }
+
 
     public void setIgv(double igv) {
         this.igv = igv;
     }
 
+
     public double getSubtotal() {
         return subtotal;
     }
+
 
     public void setSubtotal(double subtotal) {
         this.subtotal = subtotal;
     }
 
+
     public double getCostoventa() {
         return costoventa;
     }
+
 
     public void setCostoventa(double costoventa) {
         this.costoventa = costoventa;
     }
 
+
     public String getTipocomprobante() {
         return tipocomprobante;
     }
+
 
     public void setTipocomprobante(String tipocomprobante) {
         this.tipocomprobante = tipocomprobante;
     }
 
-    
 
+
+
+
+  
 }
